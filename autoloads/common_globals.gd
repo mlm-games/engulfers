5d4fcdf0bc46853044a121066417eafada002bfa
mlm-games <125530737+mlm-games@users.noreply.github.com>
2025-07-20 23:24:24 +0530

    try adding another check for restart, since the prev one didnt work

--- autoloads/common_globals.gd
+++ autoloads/common_globals.gd
@@ -11,13 +11,13 @@ func _input(event):
 				elif DisplayServer.window_get_mode() == DisplayServer.WINDOW_MODE_EXCLUSIVE_FULLSCREEN:
 					DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_WINDOWED)
 			
-			if event.is_action_pressed("restart") and event.pressed and !cam_event_running and is_instance_valid(BaseLevel.I):
-				cam_event_running = true
-				var cam_t := GameFeel.animate_to_offscreen(BaseLevel.I.camera_2d, Vector2(0, DisplayServer.screen_get_size().y * -3.25), 1.5)
-				cam_t.tween_callback(func(): 
-					A.tree.reload_current_scene()
-					cam_event_running = false
-					)
+			#if event.is_action_pressed("restart") and event.pressed and !cam_event_running and is_instance_valid(BaseLevel.I):
+				#cam_event_running = true
+				#var cam_t := GameFeel.animate_to_offscreen(BaseLevel.I.camera_2d, Vector2(0, DisplayServer.screen_get_size().y * -3.25), 1.5)
+				#cam_t.tween_callback(func(): 
+					#A.tree.reload_current_scene()
+					#cam_event_running = false
+					#)
 				
 			if OS.is_debug_build():
 				if event.keycode == KEY_EQUAL and event.pressed: #KEY_PLUS doesnt work for some reason
--- game/scenes/levels/base_level.gd
+++ game/scenes/levels/base_level.gd
@@ -1,6 +1,7 @@
 class_name BaseLevel extends Node2D
 
 static var I : BaseLevel #Current instance on screen
+var cam_t : Tween
 
 func _init() -> void:
 	I = self
@@ -10,10 +11,17 @@ func _init() -> void:
 @onready var camera_2d: Camera2D = %Camera2D
 
 func _ready() -> void:
-	GameFeel.animate_from_offscreen(%Camera2D, Vector2(0, DisplayServer.screen_get_size().y * 3.25), 1.5)
+	cam_t = GameFeel.animate_from_offscreen(%Camera2D, Vector2(0, DisplayServer.screen_get_size().y * 3.25), 1.5)
 
 
 func complete_level():
 	print("Level completed: " + str(level_id))
 	
 	LevelManager.I.advance_to_next_level()
+
+func _input(event: InputEvent) -> void:
+	if event.is_action_pressed("restart") and event.is_pressed():
+		if !cam_t.is_running():
+			cam_t = GameFeel.animate_to_offscreen(%Camera2D, Vector2(0, -DisplayServer.screen_get_size().y * 3.25), 2.5)
+			await cam_t.finished
+			get_tree().reload_current_scene()
